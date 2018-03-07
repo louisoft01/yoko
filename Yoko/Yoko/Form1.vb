e64c73233c622b3947053674ee5f8acdabff4dc9
louisoft01 <louisoft01@gmail.com>
2018-03-07 19:56:18 +0000

    Add files via upload

--- Yoko/Yoko/Form1.vb
+++ Yoko/Yoko/Form1.vb
@@ -6,14 +6,10 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Dim url As String
         Dim path As String
-nopath:
         url = TextBox1.Text
         path = TextBox2.Text
-        If url = ("c:\") Then
-            MsgBox("path is Not allowed")
-            GoTo nopath
-        End If
         My.Computer.Network.DownloadFile(url, path)
+        On Error Resume Next
         MsgBox("Complete")
     End Sub
 End Class
